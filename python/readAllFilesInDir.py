fbeb3314967d986613bd7b2da66dc948f386c0ac
SeungKiLeeCS <53922526+seungkilee-cs@users.noreply.github.com>
2021-11-02 23:42:03 -0500

    Update readAllFilesInDir.py

--- python/readAllFilesInDir.py
+++ python/readAllFilesInDir.py
@@ -1,19 +1,25 @@
+from os import rename
 from os import path
 from os import listdir
 
 def main():
-
     # Get current directory
-    path = "./"
+    curr_dir = "./"
 
     # all files in the directory
-    all_files = listdir(path)
+    all_files = listdir(curr_dir)
 
     # Filter Nones from comprehended list
-    file_name_list = list(filter(None, [str(f) if str(f)[-4:] == ".mp4" else None for f in all_files]))
+    file_name_list = list(filter(None, [str(f) if str(f)[-4:] == ".mkv" else None for f in all_files]))
 
-    for n, x in enumerate(file_name_list):
-        print('Index: {} File: {}'.format(n, x))
+    for idx, file_name in enumerate(file_name_list):
+        # print('Index: {} File: {}'.format(f'{n+1:02}', x))
+        out_dir = "Digimon Adventure꞉ (2020) S01E" + f'{idx+1:02}' + " [1080p].mkv"
+
+        src = path.realpath(file_name)
+        # print("src: {} | out: {}".format(src, out_dir))
+        rename(src, out_dir)
+
 
     # for n in nums:
     #     file_name = "[☆BJ루피☆] 파워디지몬 "+ str(n) + "화 우리말 더빙.mp4"
