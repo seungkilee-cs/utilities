8fafc7c974f318f3a05c7651f3cde054670a8e9b
SeungKiLeeCS <53922526+seungkilee-cs@users.noreply.github.com>
2021-11-10 23:03:57 -0600

    Update convert_and_rename.sh

--- bash/convert_and_rename.sh
+++ bash/convert_and_rename.sh
@@ -1,16 +1,25 @@
 ITER=1
-TITLE="TITLE"
-YEAR="2021"
+TITLE="잔혹한 관객들"
+YEAR="2017"
 SEASON="01"
 ORIG=".ts"
 EXTENSION=".mp4"
 
 for i in *${ORIG}; do
+    # Convert to new Extension with FFMPEG
     ffmpeg -i "$i" -codec copy "${i%.*}${EXTENSION}"
+
+    echo "Changing File Name ..."
+    # Get index for Naming
     IDX=$(printf "%02d" ${ITER})
+    # Save original file name with index
     echo ${i} ${IDX} >> "Original File Names.txt"
+    # Create new file name
     NEW="${TITLE} (${YEAR}) S${SEASON}E${IDX}${EXTENSION}"
-    mv "${i%.*}.mp4" "${NEW}"
+    # Rename created file
+    mv "${i%.*}${EXTENSION}" "${NEW}"
+    # Print the new name for sanity check
     echo "New Name: ${NEW}"
+    # Increment iter
     let ITER=${ITER}+1
 done
