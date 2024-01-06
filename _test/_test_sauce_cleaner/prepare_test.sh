69aa4805507e5999f043c1ef36c40e2289c58c2c
Seung Ki Lee <seungkilee.cs@gmail.com>
2024-01-06 11:58:06 +0900

    auto deploy script

--- _test/_test_sauce_cleaner/prepare_test.sh
+++ _test/_test_sauce_cleaner/prepare_test.sh
@@ -1,5 +1,5 @@
-./clean_test_files.sh -d "_nested/dir1" -e "txt"
-./clean_test_files.sh -d "_nested/dir2" -e "txt"
+./clean_test_files.sh -d "_testfiles/dir1" -e "txt"
+./clean_test_files.sh -d "_testfiles/dir2" -e "txt"
 
-./create_test_files.sh -d "_nested/dir1"
-./create_test_files.sh -d "_nested/dir2"
+./create_test_files.sh -d "_testfiles/dir1"
+./create_test_files.sh -d "_testfiles/dir2"
--- _test/_test_sauce_cleaner/run_test.sh
+++ _test/_test_sauce_cleaner/run_test.sh
@@ -1,4 +1,4 @@
-# ./test_remove_brackets.sh -d _nested/dir1
-# ./test_remove_brackets.sh -d _nested/dir2
+# ./test_remove_brackets.sh -d _testfiles/dir1
+# ./test_remove_brackets.sh -d _testfiles/dir2
 
-./wrapper_sauce_cleaner.sh -d _nested
+./wrapper_sauce_cleaner.sh -d _testfiles
